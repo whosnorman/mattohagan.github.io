638584075c13a34c0944debb11fcd618d3613249
Matt O'Hagan <mco13@my.fsu.edu>
2014-08-08 15:56:17 -0400

    added jot.html

--- js/spark.js
+++ js/spark.js
@@ -1,7 +1,5 @@
 $(document).ready(function() {
-	console.log("wat");
 	var baseURL = "https://api.spark.io/v1/devices/";
-	var timer1, timer2, timer3, timer4;
 
 	// check settings header if we don't have info
 	checkSettings();
@@ -17,12 +15,12 @@ $(document).ready(function() {
   	$( "api-token" ).on("change", function() {
   		localStorage.setItem("api-token", $("#api-token").val());
 	  	checkSettings();
-	  	console.log("api token changed");
+	  	//console.log("api token changed");
 	  });
 	$( "#core-id" ).on("change", function() {
     	localStorage.setItem("core-id", $("#core-id").val());
     	checkSettings();
-    	console.log("core id changed");
+    	//console.log("core id changed");
     });
 
     function checkSettings() {
@@ -39,26 +37,26 @@ $(document).ready(function() {
     }
 
     // alerts
-    $("#info-alert").alert();
-    $("#info-alert").affix();
+    //$("#info-alert").alert();
+    //$("#info-alert").affix();
 
     // methods
     function onMethodSuccess() {
-    	alertt = $("#alertt");
-    	alertt.animate({color: "#33D633"}, 500);
-    	alert = $("#info-alert");
+    	header = $("#header");
+    	header.animate({color: "#33D633"}, 500);
+    	//alert = $("#info-alert");
     	//alert.text("Yus").removeClass("alert-danger").addClass("alert-success");
-    	alert.show();
-    	setTimeout(function() { alertt.animate({color: "#fff"}, 1000); alert.hide(); console.log("hide"); }, 1500);
+    	//alert.show();
+    	setTimeout(function() { header.animate({color: "#fff"}, 1000); /*alert.hide(); console.log("hide"); */}, 1500);
     }
 
     function onMethodFailure() {
-    	alertt = $("#alertt");
-    	alertt.animate({color: "#FF3333"}, 500);
-    	alert = $("#info-alert");
+    	header = $("#header");
+    	header.animate({color: "#FF3333"}, 500);
+    	//alert = $("#info-alert");
     	//alert.text("nope").removeClass("alert-success").addClass("alert-danger");
-    	alert.show();
-    	setTimeout(function() { alertt.animate({color: "#fff"}, 1000); alert.hide(); console.log("hide"); }, 1500);
+    	//alert.show();
+    	setTimeout(function() { header.animate({color: "#fff"}, 1000); /*alert.hide(); console.log("hide");*/ }, 1500);
     }
 
     function doMethod(method, data) {
@@ -76,11 +74,11 @@ $(document).ready(function() {
 
     $("#post-1").on("click", function () {
 	    doMethod($("#method-1").val(), $("#data-1").val());
-	    console.log("click");
+	    //console.log("click");
 	});
 	$("#post-2").on("click", function () {
 	    doMethod($("#method-2").val(), $("#data-2").val());
-	    console.log("click");
+	    //console.log("click");
 	});
 
 
