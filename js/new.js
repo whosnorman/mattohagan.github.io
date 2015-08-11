b7e8a0536202e1265b67098ae00093a9d474f396
Matt O'Hagan <mco13@my.fsu.edu>
2015-08-10 21:15:44 -0400

    move old site to new folder, make huge change towards new site

--- js/new.js
+++ js/new.js
@@ -2,6 +2,28 @@ $(document).ready(function(){
 	$('.leftSide').addClass('leftShow');
 	$('.dontshow').addClass('show');
 	setTimeout(function(){
-		$('canvas').addClass('canvasshow');
+		$('canvas').css({opacity: 1});
+		$('.centered').css({opacity: 1});
 	}, 2000);
+
+	$(document).scroll(function(){
+        var top = $(this).scrollTop();
+        console.log(top);
+        if(top >= 100){
+        	hideLanding();
+        } else 
+        	showLanding();
+    });
+
+    function hideLanding(){
+        $('canvas').css({opacity: 0});
+        $('.nav').addClass('showNav');
+        $('.top').addClass('hideTop');
+    }
+
+    function showLanding(){
+    	$('canvas').css({opacity: 1});
+        $('.nav').removeClass('showNav');
+        $('.top').removeClass('hideTop');
+    }
 });
