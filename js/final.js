b51c774ca7dd74e8c2bf132f3f16dd48fcd5def9
Matt O'Hagan <mco13@my.fsu.edu>
2015-09-05 13:49:03 -0400

    start work on third portfolio attempt

---
+++ js/final.js
@@ -0,0 +1,9 @@
+
+$(document).ready(function(){
+	/* var notify = $('#notifyText');
+	$('.alone-nav a').hover(function(){
+		$(notify).addClass('show');
+	},function(){
+		$(notify).removeClass('show');
+	}); */
+});
