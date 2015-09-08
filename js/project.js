9e3a37d33b9d3bc89a7b2ea3937d35529a8e5e3a
Matt O'Hagan <mco13@my.fsu.edu>
2015-09-08 18:02:59 -0400

    add back button to hackfsu page

--- js/project.js
+++ js/project.js
@@ -1,4 +1,10 @@
 $(document).ready(function(){
 	$('.header-layout').hide().fadeIn(1000);
 	$('.project-content').hide().fadeIn(1500);
+
+	$('#back').on('click', function(){
+		$('body').fadeOut(1000, function(){
+			window.location.href = 'index.html';
+		});
+	});
 });
